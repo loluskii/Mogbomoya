0d0018f1ab15d9b6c9f382f45fd8e1a4341c50fb
Lolu Adelore <adeloreisaac@gmail.com>
2021-06-29 12:47:23 +0100

    update

--- resources/views/auth/sign-up.blade.php
+++ resources/views/auth/sign-up.blade.php
@@ -25,12 +25,17 @@
             overflow-y: hidden;
         }
 
-        a{
+        p a{
             text-decoration: none;
-            color: black;
+            color:#008A69;
+            font-weight: bold;
         }
         a:hover{
             text-decoration: none;
+            color:#008A69;
+        }
+        input::placeholder{
+            font-size: 14px;
         }
 
     </style>
@@ -43,26 +48,49 @@
         </div>
         <div class="col-md-6">
             <div class="container p-5 mt-5">
-                <h3 style="font-weight: bold">Welcome back!👋🏾</h3>
-                <p class="text-muted">Hey there! We've missed you.</p>
-                <form action="" class="pt-4">
-                    <div class="col-sm-12 col-md-9 px-0">
-                        <div class="form-group pb-3">
-                            <label for="">username/email</label>
-                            <input class="form-control form-control-lg" type="text" name="email" id="">
+                <h3 style="font-weight: bold">Welcome to Mogbomoya👋🏾</h3>
+                <p class="text-muted">Already have an account? <a href="">Sign In</a></p>
+                <form action="" class="pt-4" autocomplete="off">
+                    <div class="col-sm-12 col-md-10 px-0">
+                        <div class="form-row">
+                            <div class="col">
+                                <div class="form-group pb-2">
+                                    <label for="">full name</label>
+                                    <input class="form-control form-control-lg" placeholder="John Appleseed" type="text" name="email" id="">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-group pb-3">
+                                    <label for="">pick a username</label>
+                                    <input class="form-control form-control-lg" placeholder="@appleseed" type="text" name="email" id="" >
+                                </div>  
+                            </div>  
                         </div>
-                        <div class="form-group pb-3">
+
+                        <div class="form-row">
+                            <div class="col">
+                                <div class="form-group pb-2">
+                                    <label for="">email address</label>
+                                    <input class="form-control form-control-lg" placeholder="youremail@address.com" type="text" name="email" id="">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-group pb-2">
+                                    <label for="">phone number</label>
+                                    <input class="form-control form-control-lg" placeholder="2349-012-3456" type="text" name="email" id="">
+                                </div>  
+                            </div>  
+                        </div>
+                        <div class="form-group pb-2">
                             <label for="">password</label>
-                            <input class="form-control form-control-lg" type="text" name="email" id="">
-                        </div>
+                            <input class="form-control form-control-lg" placeholder="**********" type="text" name="email" id="">
+                        </div>  
                         <div class="form-row justify-content-between pb-4">
                             <div class="custom-control custom-checkbox my-1 mr-sm-2 ml-1">
                                 <input type="checkbox" class="custom-control-input" id="customControlInline">
-                                <label class="custom-control-label pt-1" for="customControlInline">Remember me</label>
+                                <label class="custom-control-label pt-1" for="customControlInline">Accept terms and condition <sup>*</sup></label>
                               </div>
-                            <div class="custom-control custom-checkbox my-1 mr-sm-2">
-                                <a href="">Forgot Password?</a>
-                            </div>
+                            
                         </div>
                         <div class="py-4">
                             <button class="btn register d-sm-none d-none d-md-block">Continue</button>
@@ -76,12 +104,11 @@
                                 <a href="" class="mr-3 mr-sm-2"><img src="{{asset('images/icons/facebook.svg')}}" alt="" srcset=""></a>
                                 <a href="" class="mr-3 mr-sm-2"><img src="{{asset('images/icons/twitter.svg')}}" alt="" srcset=""></a>
                             </p>
-                            <p class="text-sm-left text-center">Dont't have an account? <a href="">Sign up</a></p>
                         </div>
                     </div>
                 </form>
             </div>
         </div>
-        <div class="col-md-1"></div>
+        <div class="col-md-1  d-sm-none d-none d-md-block"></div>
     </div>
 </div>
