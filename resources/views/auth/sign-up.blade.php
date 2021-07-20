15c43450b35ad05c95987e036efe015ac4a934c4
Lolu Adelore <adeloreisaac@gmail.com>
2021-07-20 23:24:26 +0100

    changes to button

--- resources/views/auth/sign-up.blade.php
+++ resources/views/auth/sign-up.blade.php
@@ -94,8 +94,8 @@
                             
                         </div>
                         <div class="py-4">
-                            <button class="btn register d-sm-none d-none d-md-block">Continue</button>
-                            <button class="btn btn-block register d-sm-block  d-md-none">Continue</button>
+                            <button type="submit" class="btn btnPrimary d-sm-none d-none d-md-block">Continue</button>
+                            <button type="submit" class="btn btnPrimary btn-block register d-sm-block  d-md-none">Continue</button>
                         </div>
                         <div>
                             <p class="text-center text-sm-left">Or continue with:</p>
