5545d60aeac717576d4f3d5f1048d9456a232b80
Mikkycody <horluwatowbeey@gmail.com>
2021-08-09 13:38:19 +0100

    Quick fix

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -110,7 +110,7 @@ class EventController extends Controller
                 $tier->limit_remaining -= (int)$tier['value'];
                 $tier->update();
 
-                return $tier.(int)$tier['value']
+                return $tier.(int)$tier['value'];
             }
             $user_details = $response['data']['metadata']['user_details'];
             $registration = new EventRegistration;
