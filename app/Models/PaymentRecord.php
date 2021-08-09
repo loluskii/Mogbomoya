113c1849349001a7d4601844c235326ccd521155
Mikkycody <horluwatowbeey@gmail.com>
2021-08-09 14:41:11 +0100

    Quick fix

--- app/Models/PaymentRecord.php
+++ app/Models/PaymentRecord.php
@@ -18,4 +18,9 @@ class PaymentRecord extends Model
     {
         return $this->belongsTo(EventRegistration::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
