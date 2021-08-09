0979e0ac61976e30c755817af9bad15ea65c8897
Mikkycody <horluwatowbeey@gmail.com>
2021-08-09 14:43:48 +0100

    Quick fix

--- resources/views/user/event-info.blade.php
+++ resources/views/user/event-info.blade.php
@@ -150,23 +150,25 @@
                 </div>
             </div>
         </div>
-        <div class="container-fluid">
-            <p class="text-dark mb-0" style="font-weight: bold">Other events you may like:</p>
-        </div>
-        <div class="row flex-nowrap overflow-auto hideScrollbar pt-3 pb-5">
-            @foreach ($similarEvents as $event)
-            <div class="col-md-3 col-lg-3 col-xl-3 col-12 w-100">
-                <div class="card" >
-                    <img src="{{asset("images/event/$event->featured_image")}}" class="card-img-top" alt="{{$event->name}}">
-                    <div class="card-body">
-                        <a href="{{route('event.info', $event->reference)}}"><h5 class="text-dark  card-title">{{ $event->name }}</h5></a>
-                        <p class="card-text mb-0 text-muted">{{$event->location}}</p>
-                        <p class="card-text text-muted"><span>{{ \Carbon\Carbon::parse($event->date)->toFormattedDateString()}}</span> | <span>{{ \Carbon\Carbon::parse($event->time)->toTimeString()}} </span></p>
+        @if ($similarEvents->count() > 0)
+            <div class="container-fluid">
+                <p class="text-dark mb-0" style="font-weight: bold">Other events you may like:</p>
+            </div>
+            <div class="row flex-nowrap overflow-auto hideScrollbar pt-3 pb-5">
+                @foreach ($similarEvents as $event)
+                <div class="col-md-3 col-lg-3 col-xl-3 col-12 w-100">
+                    <div class="card" >
+                        <img src="{{asset("images/event/$event->featured_image")}}" class="card-img-top" alt="{{$event->name}}">
+                        <div class="card-body">
+                            <a href="{{route('event.info', $event->reference)}}"><h5 class="text-dark  card-title">{{ $event->name }}</h5></a>
+                            <p class="card-text mb-0 text-muted">{{$event->location}}</p>
+                            <p class="card-text text-muted"><span>{{ \Carbon\Carbon::parse($event->date)->toFormattedDateString()}}</span> | <span>{{ \Carbon\Carbon::parse($event->time)->toTimeString()}} </span></p>
+                        </div>
                     </div>
                 </div>
+                @endforeach
             </div>
-            @endforeach
-        </div>
+        @endif
 
     </div>
 
