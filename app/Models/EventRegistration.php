bb7636e75337c60bbdc3b07b26d3f5a4588a7b31
Mikkycody <horluwatowbeey@gmail.com>
2021-08-09 14:48:42 +0100

    Quick fix

--- app/Models/EventRegistration.php
+++ app/Models/EventRegistration.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EventRegistration extends Model
 {
-    use HasFactory , SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     public function event()
     {
