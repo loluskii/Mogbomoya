feea7f655e8022d5a1af884810198d3ca6b39d1e
Lolu Adelore <adeloreisaac@gmail.com>
2021-08-27 14:05:15 +0100

    updates

--- resources/views/events/emails/invite.blade.php
+++ resources/views/events/emails/invite.blade.php
@@ -1,3 +1,154 @@
-Hi {{$nameOfInvitee}}<br/>
+{{-- Hi {{$nameOfInvitee}}<br/>
 
 {{$nameOfInvite}} is inviting you for <a href="{{route('event.info', $event->reference)}}">{{$event->name}}</a>
+ --}}
+
+<table style="min-width:100%; margin:0; padding: 0;" width="100%" bgcolor="#EEEDF2">
+    <tbody>
+        <tr>
+            <td></td>
+        </tr>
+        <tr>
+            <td>
+                <table style="margin-left:auto;margin-right:auto;border-collapse:collapse;border-spacing:0;border:0;padding:0" cellpadding="0" cellspacing="0" border="0">
+                    <tbody>
+                        <tr>
+                            <td style="margin-left:auto;margin-right:auto" align="center" bgcolor="#EEEDF2">
+                                <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:;">
+                                    <tr></tr>
+                                    <tr>
+                                        <td align="center" style="padding:0;">
+                                            <table role="presentation" style="width:602px;border-collapse:collapse;border-spacing:0;text-align:left;">
+                                                <tr>
+                                                    <td style="padding:36px 30px 42px 30px;">
+                                                        <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                                                            <tr>
+                                                                <td style="padding-bottom :20px;text-align:left" align="left" bgcolor="" width="100%" height="">
+                                                                    <h4 style="font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif;"> Event Update</h4>
+                                                                    <img src="https://ci5.googleusercontent.com/proxy/t0v0a0CiB9L8phuaNDvG_sgtCswIkSUisi0W3ipP04IFfJHx68RWRLHfQ-1IWszjuask0vJgBEQ9etoFTmpI2HEMYCGVmhCeeiHMnCAyR6Yr3MHyzGNUQvbo0i1GdNOhhQLBw-yq6JR8LUa1r98PpRmb-mQQHb3bU2pFIvsa3x80yy1zg8-C06XndTeyxvcW9QRTOPUZec-bngSKFv7lQqXpKT_nYa0u0ZWW-sltHyOCRY-EaDeAiVR9qL9J32a_lpvMIVbN6me0bxB1LKmtLQR4CMc_09Xt5mxKVZvxZL6zwa2y6N4JMlC92WG3mXhtfZRB=s0-d-e1-ft#https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F118855271%2F494181368015%2F1%2Foriginal.20201117-180128?h=400&amp;w=800&amp;auto=format%2Ccompress&amp;q=75&amp;sharp=10&amp;rect=0%2C0%2C800%2C400&amp;s=5455efacd428a5acb2b10c970163be9d"
+                                                                        title="HOLBORN SCHOLARSHIP EXAM 2021 (LAGOS CHAPTER)" alt="HOLBORN SCHOLARSHIP EXAM 2021 (LAGOS CHAPTER)" border="0" width="100%" height="auto" class="CToWUd a6T" tabindex="0">
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td style="color:#153643;">
+                                                                    <p style="font-size:16px;line-height:24px;font-family:Arial,sans-serif;">
+                                                                        Hi {{$nameOfInvitee}}, <br> <br>
+                                                                        {{$nameOfInvite}} is inviting you for <a href="{{route('event.info', $event->reference)}}">{{$event->name}}</a>. <br> Please view details of this event by clicking this <a href="{{route('event.info', $event->reference)}}">link</a>.
+                                                                        Please take note.
+                                                                    </p>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr bgcolor="#EEEDF2" >
+                            <td style="background-color:#eeedf2" bgcolor="#EEEDF2" class="m_4988197524711502657grid__col">
+                                <table style="border-collapse:collapse;border-spacing:0;border:0;padding:0;width:100%" align="center">
+                                    <tbody>
+                                        <tr></tr>
+                                        <tr bgcolor="#EEEDF2">
+                                            <td width="30" bgcolor="#EEEDF2" align="center"></td>
+                                            <td style="text-align:center;background-color:#eeedf2" align="center">
+                                                <table style="border-collapse:collapse;border-spacing:0;border:0;padding:0;width:100%;color:#1e0a3c" cellspacing="0" cellpadding="0" bgcolor="#EEEDF2" align="center">
+                                                    <tbody>
+                                                        <tr bgcolor="#EEEDF2">
+                                                            <td style="padding:0;text-align:center" align="center" bgcolor="#EEEDF2" width="100%" height=""></td>
+                                                        </tr>
+                                                        <tr bgcolor="#EEEDF2">
+                                                            <td style="padding:0;text-align:center" align="center" bgcolor="#EEEDF2" width="100%" height="">
+                                                                <div style="width:140px;margin:0 auto;text-align:center">
+                                                                    <img src="http://mogbomoya.herokuapp.com/images/logo-colored.png" title="" alt="Eventbrite" border="0" width="140" class="CToWUd"></div>
+                                                            </td>
+                                                        </tr>								
+                                                        <tr></tr>								
+                                                        <tr bgcolor="#EEEDF2">
+                                                            <td style="padding:0;text-align:center" align="center" bgcolor="#EEEDF2" width="100%" height="">
+                                                                <span class="social-logo-container" style="padding:0;display:inline-block;height:auto;margin:0;width:40px;text-align:center">
+                                                                    <a href="#">
+                                                                        <img src="https://ci6.googleusercontent.com/proxy/c1YDW_PeKE92pzi5Fd7_y6MkA0CD6OHb7h6ij27EuQYYcmxWXM7kwuVq79ybRgDtPhss57-Hyxi-z9YkVaiJuwZtq7RP62jR3_Dhj7fpmCKZKMuuy7JcV0BUlRrqGpMFDpoWtke0B2CQbSfmKWpv2ejiLlPGN_4-0n6coaT16Sj4NLuORvFtezKJldINp5I=s0-d-e1-ft#https://cdn.evbstatic.com/s3-build/148050-rc2021-08-19_16.04-824fea2/django/images/emails_2018_rebrand/TW-icon-purple@2x.png" style="height:24px;padding:0;width:24px" height="24" width="24" alt="twitter" title="twitter" border="0" class="CToWUd">
+                                                                    </a>
+                                                                </span>
+                                                                <span class="social-logo-container" style="padding:0;display:inline-block;height:auto;margin:0;width:40px;text-align:center">
+                                                                    <a href="#">
+                                                                        <img src="https://ci4.googleusercontent.com/proxy/acjy6p2a5IuBB5CH9h-4jo4taCvhldsBDWB-Q_kGBwK11-DT_iSwackS5P9OOhRGXqO5qB2i3NMSRL3Zq9SjU_h5GNnp2nGUP9vQkaRQstETiff3Xhd21URTpUNeWP5QB50eREjoWqo3XTb7r4Zlp7Eq04Ag_aY1OQ2iGuwKVzcOiwhGERZ_R1BFKC9uo2U=s0-d-e1-ft#https://cdn.evbstatic.com/s3-build/148050-rc2021-08-19_16.04-824fea2/django/images/emails_2018_rebrand/FB-icon-purple@2x.png" style="height:24px;padding:0;width:24px" height="24" width="24" alt="facebook" title="facebook" border="0" class="CToWUd">
+                                                                    </a>
+                                                                </span>
+                                                                <span class="social-logo-container" style="padding:0;display:inline-block;height:auto;margin:0;width:40px;text-align:center">
+                                                                    <a href="#">
+                                                                        <img src="https://ci4.googleusercontent.com/proxy/Rxq9hYraVEMGsV51w4_-WVcupIdEKvVhsbej4FecgF80PGDxWBhNnkUGknLCpIUBjQMoTobm1HPTvc4hlVCyI9Le5X4zMg3oMPIvEJ-2RjgQ9uzkA3HTw4jegm9zuq_DFop6jnJEbcq5trF4qdTeUFoBI_6ILy54eCSfGKi0q_82SzwQ06VkuBhVwweaXi8=s0-d-e1-ft#https://cdn.evbstatic.com/s3-build/148050-rc2021-08-19_16.04-824fea2/django/images/emails_2018_rebrand/IG-icon-purple@2x.png" style="height:24px;padding:0;width:24px" height="24" width="24" alt="instagram" title="instagram" border="0" class="CToWUd">
+                                                                    </a>
+                                                                </span>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td style="line-height:10px;font-size:10px;background-color:#eeedf2" width="600" height="10">
+                                                                <table style="line-height:10px;font-size:10px;height:10px;border-collapse:collapse;border-spacing:0;border:0;padding:0;width:100%" cellspacing="0" cellpadding="0" height="10">
+                                                                    
+                                                                    <tbody><tr>
+                                                                        <td style="line-height:20px;font-size:20px;height:20px" height="20" bgcolor="">&nbsp;</td>
+                                                                    </tr>
+                                                                </tbody></table>
+                                                            </td>
+                                                        </tr>
+                                                        <tr bgcolor="#EEEDF2">
+                                                            <td class="m_4988197524711502657footer-content" style="padding:0;text-align:center" align="center" bgcolor="#EEEDF2" width="100%" height="24">
+                                                                <span style="font-weight:normal;margin:4px 0;font-size:12px;line-height:18px;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;color:#4b4d63;font-weight:normal">
+                                                                    <span>This email was sent to <a style="text-decoration:none;color:#3f60e7;color:#008A69;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-weight:normal" href="mailto:adeloreisaac@gmail.com" target="_blank">adeloreisaac@gmail.com</a>
+                                                                    </span>
+                                                                </span>
+                                                            </td>
+                                                        </tr>
+                                                        <tr bgcolor="#EEEDF2">
+                                                            <td class="m_4988197524711502657footer-content" style="padding:0;text-align:center" align="center" bgcolor="#EEEDF2" width="100%" height="24">
+                                                                <span style="font-weight:normal;margin:4px 0;font-size:12px;line-height:18px;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;color:#4b4d63;font-weight:normal">
+                                                                     Copyright © 2021 Mogbomoya. All rights reserved.
+                                                                </span>
+                                                            </td>
+                                                        </tr>
+                                                        <tr bgcolor="#EEEDF2">
+                                                            <td class="m_4988197524711502657footer-content" style="padding:0;text-align:center" align="center" bgcolor="#EEEDF2" width="100%" height="24">
+                                                                <span style="font-weight:normal;margin:4px 0;font-size:12px;line-height:18px;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;color:#4b4d63;font-weight:normal">								
+                                                                </span>
+                                                            </td>
+                                                        </tr>
+                                                        <tr></tr>
+                                                        <tr></tr>
+                                                        <tr></tr>
+                                                        <tr style="padding: 30px;">
+                                                            <td style="line-height:18px;font-size:18px;background-color:#eeedf2" width="600" height="18">
+                                                                <table style="line-height:18px;font-size:18px;height:18px;border-collapse:collapse;border-spacing:0;border:0;padding:0;width:100%" cellspacing="0" cellpadding="0" height="18">
+                                                                    
+                                                                    <tbody><tr>
+                                                                        <td style="line-height:36px;font-size:36px;height:36px" height="36" bgcolor="">&nbsp;</td>
+                                                                    </tr>
+                                                                </tbody></table>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>
+                                                                <img src="https://ci4.googleusercontent.com/proxy/hg32yr2sRZriZFPArTDcK1_JAVi29tyMUx58QWu8MmSiNyT082V_PIhQT2p5VpdLeq42RZlaPZGjzfxqI29x2wk7IUHm8YtAJtjy4gv7_uFVuaA4GYacfu0PlciD4MFWs2fx3iIor7_rcmypjd_XW3649PPSl336Gxoe-gGhhCTocGQWYrMpgDzA4SiEGkhVIw=s0-d-e1-ft#https://www.eventbrite.com/emails/action/?recipient=adeloreisaac%40gmail.com&amp;type_id=65&amp;type=open&amp;send_id=2021-08-22&amp;list_id=9" alt="" width="1" height="1" class="CToWUd">
+                        
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                            <td width="30" bgcolor="#EEEDF2"></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
